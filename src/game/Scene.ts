077be3ec7ea00632fcd93007575f24ce5c3be09a
UstymUkhman <ustym.ukhman@gmail.com>
2020-12-16 19:19:50 +0200

    Stars background added

--- src/game/Scene.ts
+++ src/game/Scene.ts
@@ -3,6 +3,7 @@ import { Scene, GameObjects } from 'phaser';
 
 export default class extends Scene {
   private center = { x: 0.0, y: 0.0 };
+  private stars!: GameObjects.Image;
   private sky!: GameObjects.Image;
 
   public constructor () {
@@ -16,25 +17,39 @@ export default class extends Scene {
     };
 
     this.load.image('sky', 'assets/sky.png');
+    this.load.image('stars', 'assets/stars.png');
   }
 
   private create (): void {
-    this.sky = this.add.image(0, 0, 'sky');
-
     this.scale.on('resize', this.resize, this);
+    this.createEnvironment();
     this.resize();
   }
 
+  private createEnvironment (): void {
+    this.sky = this.add.image(0, 0, 'sky').setScrollFactor(0, 0.95);
+    this.stars = this.add.image(0, 0, 'stars').setScrollFactor(0, 0);
+  }
+
   private resize (): void {
+    const topLevel = background.levels - 1.5;
     const midLevel = background.levels / 2;
     const { width, height } = this.scale;
 
+    this.stars.displayHeight = width / 16 * 9;
     this.sky.displayHeight = height * 6;
+
+    this.stars.displayWidth = width;
     this.sky.displayWidth = width;
 
     this.sky.setPosition(
-      this.scale.width / 2,
+      this.center.x,
       this.sky.displayHeight / -midLevel
+    );
+
+    this.stars.setPosition(
+      this.center.x,
+      this.stars.displayHeight * -topLevel
     );
   }
 };
